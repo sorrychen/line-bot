099d99d74bc54d48e6fa6aaee0c2e6281e8440f0
sorrychen <civil.pianist@gmail.com>
2025-03-28 02:23:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def callback():
 
     return "OK"
 
-@handler.add(MessageEvent, message=TextMessageContent)
+@line_handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     user_message = event.message.text
     add_user_message_to_thread(client, thread_id, user_message)
